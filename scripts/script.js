4692b751eb775454ab00a00205b08902eba92cf9
João Pedro Rodrigues <joaopedrorodrigues1333@gmail.com>
2025-09-01 14:34:12 -0300

    Coloquei algumas animações e alguns ajustes.

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,42 @@ $(document).ready(function() {
         var icon = $('#mobile_btn').find("i");
         icon.toggleClass('fa-bars fa-x');
     });
+
+    let ultimoScroll = window.scrollY;
+    const header = document.getElementById('cabecalho');
+
+    window.addEventListener('scroll', () => {
+        const scrollAtual = window.scrollY;
+        if (window.innerWidth <= 1170)
+        {
+            if (scrollAtual > ultimoScroll)
+                header.classList.add('escondido');
+            else
+                header.classList.remove('escondido');
+        }
+        ultimoScroll = scrollAtual;
+    });
+
+    ScrollReveal().reveal('#quem_sou_eu', {
+        origin: 'top',
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        reset: false
+    })
+    ScrollReveal().reveal('#img_abordagem', {
+        origin: 'left',
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        reset: false
+    })
+    ScrollReveal().reveal('.pessoa', {
+        origin: 'right',
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        interval: 500,
+        reset: false
+    })
 });
